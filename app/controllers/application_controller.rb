4c45cd3733e8bae36d9e6c947006470e359653e5
Myles Iribarne <myles.iribarne@gmail.com>
2020-07-19 14:07:35 -0700

    finish homework

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < ActionController::Base
   layout(false)
 
+  def homepage 
+    render({:template=>"game_templates/homepage.html.erb"})
+  end
+
+  def play_rock 
+    render({:template=>"game_templates/user_rock.html.erb"})
+  end
+
+  def play_paper 
+    render({:template=>"game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors 
+    render({:template=>"game_templates/user_scissors.html.erb"})
+  end
+
+
   # Add your actions below this line
   # ================================
 
